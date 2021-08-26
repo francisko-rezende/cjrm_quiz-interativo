8935d15ed0ba97cf5ebb6c364ded8b609277c381
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2021-08-26 16:48:37 -0300

    refactors app.js

--- app.js
+++ app.js
@@ -2,35 +2,59 @@ const form = document.querySelector('.quiz-form')
 const resultPopupWrapper = document.querySelector('.popup-wrapper')
 const resultSentence = document.querySelector('#result-sentence')
 
-const correctAnswers = ['B', 'B', 'B', 'B']
 
-form.addEventListener('submit', event => {
-    event.preventDefault()
-    
-    const userAnswers = [
+const correctAnswers = ['B', 'B', 'B', 'B']
+let score = 0
+
+
+const getUserAnswers = () => {
+    return [
         form.inputQuestion1.value,
         form.inputQuestion2.value,
         form.inputQuestion3.value,
         form.inputQuestion4.value,
     ]
+}
 
-    let score = 0
+const checkAnswers = (userAnswer, index) => {
+    const isAnswerCorrect = userAnswer === correctAnswers[index]
+    const numberOfQuestions = correctAnswers.length 
+    const maxScore = 100
+    const pointsForGettingAnAnswerRight = maxScore / numberOfQuestions  
+    
+    if (isAnswerCorrect) {
+        score += pointsForGettingAnAnswerRight
+    }
+}
 
-    userAnswers.forEach((userAnswer, index) => {
-        if (userAnswer === correctAnswers[index]) {
-            score += 100 / correctAnswers.length
-        }
-    })
-
-    resultSentence.textContent = `Sua pontuação foi ${100 / correctAnswers.length}/100`
+const showResult = () => {
+    resultSentence.textContent = `Sua pontuação foi ${score}/100`
     resultPopupWrapper.style.display = 'block'
-})
+}
 
-resultPopupWrapper.addEventListener('click', event => {
+const gradeQuizAndShowResult = event => {
+    event.preventDefault()
+    
+    const userAnswers = getUserAnswers()
+    score = 0
+
+    userAnswers.forEach(checkAnswers)
+
+    showResult()
+}
+
+const closeResultPopup = event => {
     const clickedElementClass = event.target.classList[0]
     const classNames = ['popup-close', 'popup-wrapper', 'popup-close-button']
 
-    if (classNames.includes(clickedElementClass)) {
+    const shouldClosePopup = classNames.includes(clickedElementClass) 
+
+    if (shouldClosePopup) {
         resultPopupWrapper.style.display = 'none'
     }
-})
+}
+
+
+form.addEventListener('submit', gradeQuizAndShowResult)
+
+resultPopupWrapper.addEventListener('click', closeResultPopup)
